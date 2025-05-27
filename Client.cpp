28d991abab358516290f12fbc8140c24e8bd36ad
FranekFedorowicz <162139512+FranekFedorowicz@users.noreply.github.com>
2025-05-27 09:38:55 +0200

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,46 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <iostream>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib") 
+
+void receive_messages(SOCKET sock) {
+    char buffer[1024];
+    int bytesReceived;
+    while ((bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytesReceived] = '\0'; 
+        std::cout << "\n[Wiadomosc] " << buffer << "\nTy: "; 
+        std::cout.flush();
+    }
+    std::cout << "\nRozlaczono z serwerem.\n"; 
+}
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa); 
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);  
+    sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888); 
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    connect(sock, (sockaddr*)&server, sizeof(server)); 
+    std::cout << "Polaczono z serwerem!\n";
+
+    std::thread receiver(receive_messages, sock); 
+
+    std::string input;
+    while (true) {
+        std::cout << "Ty: ";
+        std::getline(std::cin, input); 
+        if (input == "/exit") break;
+        send(sock, input.c_str(), input.length(), 0); 
+    }
+
+    closesocket(sock);
+    receiver.detach();  
+    WSACleanup(); 
+    return 0; 
+}
---
+++ Server.cpp
@@ -0,0 +1,88 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib") 
+
+std::vector<SOCKET> clients; 
+std::mutex clients_mutex; 
+
+
+void broadcast(const std::string& message, SOCKET sender = INVALID_SOCKET) {
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    for (SOCKET client : clients) {
+        if (client != sender) {
+            send(client, message.c_str(), message.length(), 0);
+        }
+    }
+}
+
+
+void handle_client(SOCKET clientSocket) {
+    char buffer[1024];
+    int bytesReceived;
+   
+    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytesReceived] = '\0'; 
+        std::string msg = buffer;
+
+        std::cout << "[Klient] " << msg << std::endl;
+        broadcast("[Klient] " + msg, clientSocket); 
+    }
+  
+    {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+        clients.erase(std::remove(clients.begin(), clients.end(), clientSocket), clients.end());
+    }
+    closesocket(clientSocket);
+    std::cout << "Klient rozlaczony.\n";
+}
+
+void server_chat() {
+    std::string input;
+    while (true) {
+        std::getline(std::cin, input);
+        if (input == "/exit") break; 
+
+        std::string msg = "[Serwer] " + input; 
+        broadcast(msg); 
+    }
+}
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+   
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; 
+    server.sin_port = htons(8888); 
+
+    bind(serverSocket, (sockaddr*)&server, sizeof(server)); 
+    listen(serverSocket, 5); 
+
+    std::cout << "Serwer dziala. Oczekiwanie na klientow...\n";
+
+    std::thread chatThread(server_chat); 
+
+    while (true) {
+        SOCKET clientSocket = accept(serverSocket, nullptr, nullptr); 
+        {
+            std::lock_guard<std::mutex> lock(clients_mutex); 
+            clients.push_back(clientSocket); 
+        }
+        std::thread(handle_client, clientSocket).detach(); 
+        std::cout << "Nowy klient polaczony.\n";
+    }
+
+    closesocket(serverSocket);
+    WSACleanup(); 
+    return 0;
+}
